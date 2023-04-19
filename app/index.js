9027d59a643fecfbd649a7dfd3260398c938185a
Junhan <junhan@qiren.com.sg>
2023-04-19 22:58:43 +0800

    feat: SafeAreaView &&  +header stack.screen style

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router'; 
+import { COLORS, icons, images, SIZES } from '../constants';
+import { NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome } from '../components'
 
 const Home = () => {
+
     return (
-        <View>
-            <Text>Home page</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, background: COLORS.lightWhite }}>
+            <Stack.Screen options={{ 
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                ),
+                headerTitle: ""
+            }} />
+        </SafeAreaView>
     );
 }
 
