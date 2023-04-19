8306ffb335cc1af42533843c1fe9ed81d0069d8f
Junhan <junhan@qiren.com.sg>
2023-04-20 01:05:16 +0800

    feat: show our home page only if the fonts are loaded

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,31 @@
 import { Stack } from 'expo-router'
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+// make splash screen visible until hideAsync is called
+SplashScreen.preventAutoHideAsync()
+
+// we only want to show our home page if the fonts have been loaded
 
 const Layout = () => {
-    return <Stack />;
+
+    // we can now use this fonts in our application
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView} />;
 }
 
 export default Layout;
