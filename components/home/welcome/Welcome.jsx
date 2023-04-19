9de66a85e46d5683ccac42602a795ca3a0614dec
Junhan <junhan@qiren.com.sg>
2023-04-20 00:41:29 +0800

    feat: Welcome greeting and text input

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={ styles.container }>
+        <Text style={styles.userName}>Hello, Junhan</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   )
 }
