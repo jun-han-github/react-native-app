7b5919b4df65853a3cd47be3eddd8588f8e8d609
Junhan <junhan@qiren.com.sg>
2023-04-19 21:56:26 +0800

    first commit && expo setup

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home page</Text>
+        </View>
+    );
+}
+
+export default Home;
