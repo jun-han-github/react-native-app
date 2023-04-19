017e023be2faa19af06ef5173c8e9ac84b7e38a3
Junhan <junhan@qiren.com.sg>
2023-04-20 01:26:58 +0800

    feat: temp job types && horizontal listing

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
 
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
+import { useRouter } from 'expo-router';
+
+const jobTypes = ["Full-time", "Part-Time", "Contractor"];
 
 const Welcome = () => {
+
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-Time');
+
   return (
     <View>
       <View style={ styles.container }>
@@ -14,8 +21,44 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What jobs are you looking for?"
+          />
         </View>
+
+        <TouchableOpacity
+          style={styles.searchBtn} 
+          onPress={() => {}}
+        >
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+            style={styles.tab(activeJobType, item)}
+            onPress={() => {
+              setActiveJobType(item);
+              router.push(`/search/${item}`)
+            }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   )
