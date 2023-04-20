9d2d8a4e967759d14bbe4e1f1e22549fd83d9e3f
Junhan <junhan@qiren.com.sg>
2023-04-20 13:06:21 +0800

    fix: unable to read "@env" and feat: fetch data

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,23 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(false);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["module:metro-react-native-babel-preset", "babel-preset-expo"],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: false,
+          verbose: false
+        }
+      ],
     ],
   };
 };
